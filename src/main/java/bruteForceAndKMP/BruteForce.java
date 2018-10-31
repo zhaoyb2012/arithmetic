35300eeb17225332dcbc5d9910ec54e876de5a82
zhaoyongbo <zhaoyongbo@mobike.com>
2018-10-31 22:35:37 +0800

    project init

---
+++ src/main/java/bruteForceAndKMP/BruteForce.java
@@ -0,0 +1,21 @@
+package bruteForceAndKMP;
+
+public class BruteForce {
+
+    public static int bruteForce(String str,String sub){
+        int i=0,j=0,index=-1;
+        while (i<str.length() && j<sub.length()){
+            if(str.charAt(i) == sub.charAt(j)){
+                i++;
+                j++;
+            }else{
+                i=i-j+1;
+                j=0;
+            }
+            if(j==sub.length()){
+                index = str.length()-j;
+            }
+        }
+        return index;
+    }
+}
---
+++ src/main/java/bruteForceAndKMP/TestMain.java
@@ -0,0 +1,11 @@
+package bruteForceAndKMP;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        String str1 = "abcdefgh";
+        String str2 = "efa";
+
+        System.out.println(BruteForce.bruteForce(str1, str2));
+    }
+}
